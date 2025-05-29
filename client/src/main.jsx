abbc6b804c68716e6cbb82001b2720e34fda0aae
afsal-11-af <afsalmohamedece@gmail.com>
2025-05-29 15:22:41 +0530

    commited

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
-const router=createBrowserRouter([]);
+import Home from './pages/Home'
+import SignIn from './pages/SignIn.jsx'
+const router=createBrowserRouter([
+  {
+    path : '/',
+    element:<Home/>
+  },
+  {
+    path : '/login',
+    element:<SignIn/>
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
