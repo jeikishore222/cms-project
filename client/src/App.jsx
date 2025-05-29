2389063e88f365269eed757569d1bb846d340c09
dharanvignesh <dharanvignesh8@gmail.com>
2025-05-29 10:54:31 +0530

    dcommit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 
 import React from 'react';
-import Signup from './pages/Signup'
-import SignIn from './pages/SignIn'
+// import Signup from './pages/Signup'
+// import SignIn from './pages/SignIn'
+ import Develop from './pages/Develop'
 import './App.css'
 function App() {
   return (
     <>
-      <StudentDetails/>
-      <Home/>
+      <Develop/>
+      {/* <Home/>
       <Signup/>
-      <SignIn/>
+      <SignIn/> */}
     </>
   )
 }
--- client/src/pages/Develop.jsx
+++ client/src/pages/Develop.jsx
@@ -1,38 +1,39 @@
 import React from "react";
- 
-const studentData = [
-    {
-        id : 1,
-        name : 'Jeikishore K',
-        regNo : '22BEECE019',
-        course : 'Electronics And Communication Engineering',
-    },
-    {
-        id : 2,
-        name : 'Afsal Mohamed K',
-        regNo : '22BEECE001',
-        course : 'Electronics And Communication Engineering',
-    },
-    {
-        id : 3,
-        name : 'Dharan Vignesh S',
-        regNo : '22BEECE009',
-        course : 'Electronics And Communication Engineering',
-    }
+
+const developers = [
+  {
+    id: 1,
+    name: 'Jeikishore K',
+    regNo: '22BEECE019',
+    course: 'Electronics And Communication Engineering',
+  },
+  {
+    id: 2,
+    name: 'Afsal Mohamed K',
+    regNo: '22BEECE001',
+    course: 'Electronics And Communication Engineering',
+  },
+  {
+    id: 3,
+    name: 'Dharan Vignesh S',
+    regNo: '22BEECE009',
+    course: 'Electronics And Communication Engineering',
+  }
 ];
-const StudentDetails = () => {
-    return(
-        <div style={{ padding: '20px',backgroundColor: "		#ffffff" }}> 
+
+const Develop = () => {
+  return (
+    <div style={{ padding: '20px', backgroundColor: '#ffffff' }}>
       <h1>Developer Page - Student Details</h1>
-       {studentData.map((student) => (
-       <div
+      {developers.map((student) => (
+        <div
           key={student.id}
           style={{
             marginBottom: "20px",
             padding: "10px",
             border: "1px solid #ccc",
             borderRadius: "5px",
-            backgroundColor: "	#dee2e6",
+            backgroundColor: "#dee2e6",
           }}
         >
           <p><strong>Name:</strong> {student.name}</p>
@@ -41,6 +42,7 @@ const StudentDetails = () => {
         </div>
       ))}
     </div>
-    );
+  );
 };
-export default StudentDetails;
+
+export default Develop;
