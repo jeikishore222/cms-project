76f6e0d4ef151a815b44a1a667b1f5eab30a44eb
dharanvignesh <dharanvignesh8@gmail.com>
2025-05-29 10:58:55 +0530

    devcommit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import Signup from './pages/Signup'
 import SignIn from './pages/SignIn'
-import Develop from './pages/Develop';
+import Develop from './pages/Develop'
+import Home from './pages/Home'
 import './App.css'
 function App() {
   return (
